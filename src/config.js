133d037b06997c4ccaa86eecac86a0848f04075f
Josh <joshdcuneo@gmail.com>
2020-03-27 14:16:59 +1100

    work

--- src/config.js
+++ src/config.js
@@ -6,6 +6,5 @@ export const toolsLink = "https://wpcc.church/tools"
 export const talksLink = "https://wpcc.church/talks"
 export const prayerFormLink = "https://wpcc.church/forms/prayer"
 const youtubeBase = "https://www.youtube.com/embed/videoseries?list="
- export const youtubeLink =
-      youtubeBase +"PLJbXzmMjnMZJ0BdXS3pT0qXblBCrj4Gip",
- export const bibleReading = "bible/111/ISA.10.NIV",
+export const youtubeLink = youtubeBase + "PLJbXzmMjnMZJ0BdXS3pT0qXblBCrj4Gip"
+export const bibleReading = "bible/111/ISA.10.NIV"
