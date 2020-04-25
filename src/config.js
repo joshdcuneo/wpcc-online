9fc42f29378071ecbe763e8a90bb3e35652c0263
Josh <joshdcuneo@gmail.com>
2020-04-25 18:24:13 +1000

    update

--- src/config.js
+++ src/config.js
@@ -8,5 +8,5 @@ export const prayerFormLink = "https://wpcc.church/forms/prayer"
 // const youtubeBase =
 //   "https://www.youtube.com/embed?listType=playlist&rel=0&autoplay=1&list="
 // export const youtubeLink = youtubeBase +
-export const bibleReading = "bible/111/ISA.12.NIV"
-export const youtubePlaylistId = "PLJbXzmMjnMZID7T4q9rnb4MNEfeZroXSC"
+export const bibleReading = "bible/111/ROM.10.NIV"
+export const youtubePlaylistId = "PLJbXzmMjnMZJeZ9MbhQdO349jagOGqObc"
